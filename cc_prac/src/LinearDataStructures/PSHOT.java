588a217a6fdec0c9c829665635b7b1bc113bc7f5
ShivamBarke <sbarke236@gmail.com>
2023-01-26 16:07:55 +0530

    Adding Java Project

---
+++ cc_prac/src/LinearDataStructures/PSHOT.java
@@ -0,0 +1,56 @@
+package LinearDataStructures;
+/* https://www.codechef.com/LRNDSA02/problems/PSHOT */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class PSHOT {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		try {
+		    Scanner input = new Scanner(System.in);
+	        int T = input.nextInt();
+            for(int x=1;x<=T;x++){
+                int n = input.nextInt();
+                String s = input.next();
+                
+                int g_a = 0;
+                int g_b = 0;
+                int rem_a = n;
+                int rem_b = n;
+                int flag = 1;
+                for(int i=0;i<2*n;i++){
+                    if(i%2==0){
+                        //A's shot
+                        if(s.charAt(i)=='1'){
+                        g_a++;
+                    }
+                        rem_a--;
+                    } else {
+                        //B's shot
+                        if(s.charAt(i)=='1'){
+                        g_b++;
+                    }
+                        rem_b--;
+                    }
+                    if (g_a-g_b> rem_b) {
+                        System.out.println(i+1);
+                        flag = 0;
+                        break;
+                    } else if (g_b-g_a>rem_a) {
+                        System.out.println(i+1);
+                        flag = 0;
+                        break;
+                    }
+                }
+                if (flag==1) {
+                    System.out.println(2*n);
+                }
+            
+	    }
+	    input.close();
+		}
+	    catch(Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+}
---
+++ cc_prac/src/LinearDataStructures/STFOOD.java
@@ -0,0 +1,30 @@
+package LinearDataStructures;
+/* https://www.codechef.com/LRNDSA02/problems/STFOOD */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class STFOOD {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		try {
+		    Scanner input = new Scanner(System.in);
+	        int T = input.nextInt();
+            for(int i=1;i<=T;i++){
+                int n = input.nextInt();
+                int[] profit = new int[101];
+                for(int x=1;x<=n;x++){
+                    int s = input.nextInt();
+                    int p = input.nextInt();
+                    int v = input.nextInt();
+                    int pro = (p/(s+1))*v;
+                    profit[x] = pro;
+                }
+            System.out.println(Arrays.stream(profit).max().getAsInt());
+	    }
+	    input.close();
+		}
+	    catch(Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+}
