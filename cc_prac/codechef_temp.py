031ef4050207febf47ef76236813c001ffad990d
ShivamBarke <sbarke236@gmail.com>
2023-01-30 23:38:15 +0530

    made codechef template for py

---
+++ cc_prac/codechef_temp.py
@@ -0,0 +1,26 @@
+# https://docs.python.org/3/library/collections.html
+from collections import *
+
+# https://docs.python.org/3/library/math.html
+from math import ceil, floor, fsum, gcd, lcm, factorial, perm, comb, log, log2, log10, pow, sqrt
+
+def inp():
+    return input().rstrip()
+
+def intinp():
+    return int(input().rstrip())
+
+def mapintinp():
+    return map(int, input().split())
+
+def lstintinp():
+    return list(map(int, input().split()))
+
+def lstinp():
+    return list(input().split())
+
+def total(iterable):
+    return fsum(iterable)
+
+
+
